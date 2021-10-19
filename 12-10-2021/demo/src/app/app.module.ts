b3ad543698bd41231e44c635a582f4ddc7eba706
Riya Jain <riyajn2001@gmail.com>
2021-10-19 17:42:44 +0530

    Added q1 for assignment

--- 12-10-2021/demo/src/app/app.module.ts
+++ 12-10-2021/demo/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
 import { LoginComponent } from './login/login.component';
+import { AssignmentComponent } from './assignment/assignment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DemoComponent,
-    LoginComponent
+    LoginComponent,
+    AssignmentComponent
   ],
   imports: [
     BrowserModule,
