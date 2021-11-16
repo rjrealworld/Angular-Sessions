d6c1f3886ac467233ff110f4da7f7ede89f2df6e
Riya Jain <riyajn2001@gmail.com>
2021-11-16 21:19:54 +0530

    Added express api

--- 12-10-2021/NodeJS/app/app.js
+++ 12-10-2021/NodeJS/app/app.js
@@ -1,9 +1,14 @@
 //create an api using node js
 
+/*
 //import http object
 var http = require('http');
 
 var myServer = http.createServer(function(req, res) {//req carries data & url coming from client and res send output to client
+    // some browsers might not support html code
+    // write the below line for that
+    // res.writeHeader(200, {'Content-Type': 'text/html'});
+    
     // res.write('Welcome to Node JS APIs.');
     // whatever we write inside .write() is HTML code
     res.write('<h1><center>Welcome to Node JS HTML file</center></h1>');
@@ -12,6 +17,7 @@ var myServer = http.createServer(function(req, res) {//req carries data & url co
 
 myServer.listen(3000); //3000 is the default port number
 console.log('Server is running on port 3000');
+*/
 
 // --------------------------------------------------
 // You need to restart the server everytime you make changes to the code.
@@ -22,3 +28,11 @@ console.log('Server is running on port 3000');
 // Command to run this file: nodemon app.js
 // --------------------------------------------------   
 // to work with express js (framework), install express by command: npm install express
+
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) {
+    res.send('<h1><center>Welcome to Express JS API</center></h1>');
+});
+app.server = app.listen(3000);
+console.log('Server is running on port 3000');
