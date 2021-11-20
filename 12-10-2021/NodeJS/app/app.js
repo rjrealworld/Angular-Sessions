0173c7c1c37baa14834c27e8309b6220f6122025
Riya Jain <riyajn2001@gmail.com>
2021-11-20 20:52:26 +0530

    Added routing for employees data:

--- 12-10-2021/NodeJS/app/app.js
+++ 12-10-2021/NodeJS/app/app.js
@@ -4,6 +4,9 @@ var app = express();
 app.set("port", process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
+    res.send(`<h3>Welcome to Node JS Routing</h3>`);
+});
+app.get('/employees', function(req, res) {
     var result = "";
     dataFile.employees.forEach(employee => {
         result += `
@@ -15,6 +18,15 @@ app.get('/', function(req, res) {
         `
     });
     res.send(`${result}`);
+});
+app.get('/employees/:index', function(req, res) {
+    var index = req.params.index;
+    var employee = dataFile.employees[index];
+    res.send(`
+        ${employee.empId}
+        ${employee.empName}
+        ${employee.salary}
+    `);
 });
 app.server = app.listen(app.get("port"));
 console.log('Server is running on port: ' + app.get('port'));
