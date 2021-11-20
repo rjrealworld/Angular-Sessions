462c7608423886f06435a1b9fbf6e35fb6e19207
Riya Jain <riyajn2001@gmail.com>
2021-11-20 20:36:17 +0530

    Changed the port dynamically and added json file in the app.js

--- 12-10-2021/NodeJS/app/app.js
+++ 12-10-2021/NodeJS/app/app.js
@@ -1,38 +1,20 @@
-//create an api using node js
-
-/*
-//import http object
-var http = require('http');
-
-var myServer = http.createServer(function(req, res) {//req carries data & url coming from client and res send output to client
-    // some browsers might not support html code
-    // write the below line for that
-    // res.writeHeader(200, {'Content-Type': 'text/html'});
-    
-    // res.write('Welcome to Node JS APIs.');
-    // whatever we write inside .write() is HTML code
-    res.write('<h1><center>Welcome to Node JS HTML file</center></h1>');
-    res.end();
-});
-
-myServer.listen(3000); //3000 is the default port number
-console.log('Server is running on port 3000');
-*/
-
-// --------------------------------------------------
-// You need to restart the server everytime you make changes to the code.
-// Command to run this file: node app.js
-// --------------------------------------------------
-// In order to restart the server itself on each change, 
-// you need to install nodemon by command: npm install -g nodemon
-// Command to run this file: nodemon app.js
-// --------------------------------------------------   
-// to work with express js (framework), install express by command: npm install express
-
+var dataFile = require('../data/data.json');
 var express = require('express');
 var app = express();
+app.set("port", process.env.PORT || 3000);
+
 app.get('/', function(req, res) {
-    res.send('<h1><center>Welcome to Express JS API</center></h1>');
+    var result = "";
+    dataFile.employees.forEach(employee => {
+        result += `
+            <li>
+                <h2>${employee.empId}</h2>
+                <h2>${employee.empName}</h2>
+                <h2>${employee.salary}</h2>
+            </li>
+        `
+    });
+    res.send(`${result}`);
 });
-app.server = app.listen(3000);
-console.log('Server is running on port 3000');
+app.server = app.listen(app.get("port"));
+console.log('Server is running on port: ' + app.get('port'));
